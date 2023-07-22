b28d097277000904153a3086f0028dd3edb50aa1
Jrcity <redemptionjonathan1@gmail.com>
2023-07-22 01:30:52 +0100

    Readjusting imports for the previous commit msg

--- src/pages/orders/[slug].tsx
+++ src/pages/orders/[slug].tsx
@@ -1,28 +1,31 @@
 /* eslint-disable react/display-name */
 /* eslint-disable import/no-anonymous-default-export */
 import { useRouter } from "next/router";
-import { OrderDetails } from "./OrderDetails";
-import { OrderInvoice } from "./OrderInvoice";
+import OrderDetails from "./OrderDetails";
+import OrderInvoice from "./OrderInvoice";
 
 export default function () {
-  const {
-    query: { slug },
-  } = useRouter();
+	const {
+		query: { slug },
+	} = useRouter();
 
-  if (slug === "details") return <OrderDetails />;
-  if (slug == "invoice") return <OrderInvoice />;
+	if (slug === "details") return <OrderDetails />;
+	if (slug == "invoice") return <OrderInvoice />;
 }
 
 export function getStaticPaths() {
-  return {
-    paths: [{ params: { slug: "details" } }, { params: { slug: "invoice" } }],
-    fallback: false,
-  };
+	return {
+		paths: [
+			{ params: { slug: "details" } },
+			{ params: { slug: "invoice" } },
+		],
+		fallback: false,
+	};
 }
 export function getStaticProps() {
-  return {
-    props: {
-      slug: "",
-    },
-  };
+	return {
+		props: {
+			slug: "",
+		},
+	};
 }
