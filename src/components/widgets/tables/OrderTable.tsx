f556e01c13c2ddb17480d173fc3e4155330449f1
Jrcity <redemptionjonathan1@gmail.com>
2023-09-24 21:23:50 +0100

    momentary push

--- src/components/widgets/tables/OrderTable.tsx
+++ src/components/widgets/tables/OrderTable.tsx
@@ -1,9 +1,7 @@
 import { IOrder } from "@/interfaces/tables.interface";
-import * as ScrollArea from "@radix-ui/react-scroll-area";
 import {
 	ColumnDef,
 	SortingState,
-	Table,
 	flexRender,
 	getCoreRowModel,
 	getFilteredRowModel,
@@ -13,7 +11,7 @@ import {
 } from "@tanstack/react-table";
 import { FC, memo, useContext, useMemo, useState } from "react";
 import { useRouter } from "next/router";
-import { BiCaretRight, BiChevronDown, BiChevronUp } from "react-icons/bi";
+import { BiChevronDown, BiChevronUp } from "react-icons/bi";
 import { MdRemoveRedEye, MdSearch } from "react-icons/md";
 import Image from "next/image";
 
@@ -23,14 +21,11 @@ import { SelectPicker } from "../SelectPicker";
 import { IOrderContext, OrdersContext } from "@/contexts/OrdersProvider";
 import { ResultsFallback } from "../ResultsFallback";
 import { formattedDate } from "@/utils/formatted_date";
-import { RxChevronLeft, RxChevronRight, RxDoubleArrowLeft, RxDoubleArrowRight } from "react-icons/rx";
-// import { useSelector } from "react-redux";
-// import { RootState } from "@/types/store.type";
+import { createPaginationWithCustomType } from "@/utils/createPaginationWithCustomType";
 
 export const OrderTable: FC = memo(() => {
 	const router = useRouter();
 	const { selectedFilter } = useContext(OrdersContext) as IOrderContext;
-	// const { orders } = useSelector((state: RootState) => state.user);
 	const [sorting, setSorting] = useState<SortingState>([]);
 	const [data, setData] = useState(orderData);
 
@@ -191,6 +186,7 @@ export const OrderTable: FC = memo(() => {
 		getCoreRowModel: getCoreRowModel(),
 		getFilteredRowModel: getFilteredRowModel(),
 		getPaginationRowModel: getPaginationRowModel(),
+		debugTable: true,
 	});
 	if (!data.length) {
 		return <ResultsFallback />;
@@ -198,164 +194,117 @@ export const OrderTable: FC = memo(() => {
 
 	return (
 		<main className="relative">
-		<div className="my-10 w-full  h-[85vh] pb-2 bg-white overflow-auto rounded-md border shadow-sm border-slate-300">
-			<header className="flex sticky top-0 justify-between items-center border-b border-slate-300 text-afruna-blue bg-white ">
-				<h1 className="p-3 font-bold ">Orders</h1>
-				<div className="flex justify-between items-center p-3 space-x-2">
-					<InputLabel
-						getValue={(val) => console.log("")}
-						placeholder={"Search"}
-						inputClassName="text-sm p-1"
-						inputsuffixIcon={
-							<button>
-								<MdSearch />
-							</button>
-						}
-						type={"text"}
-					/>
-					<SelectPicker
-						triggerClassName="relative top-[5px] flex w-24 justify-between items-center space-x-1 text-[12px] p-[11px] border border-afruna-gray/30 rounded-md"
-						placeholder="Months"
-						getSelected={(val) => console.log(val)}
-						items={["All", "January", "Febuary", "March", "April"]}
-					/>
-					<SelectPicker
-						triggerClassName="relative top-[5px] flex w-24 justify-between items-center space-x-1 text-[12px] p-[11px] border border-afruna-gray/30 rounded-md"
-						placeholder="Select"
-						getSelected={(val) => console.log(val)}
-						items={[
-							"This Month",
-							"3 Days",
-							"1 Week",
-							"2 Months",
-							"6 Months",
-						]}
-					/>
-				</div>
-			</header>
-			<table className="w-screen lg:w-full px-8 relative">
-				<thead className=" sticky top-16 bg-white">
-					{table.getHeaderGroups().map((headerGroup) => (
-						<tr
-							className="text-left text-afruna-gray text-[12px] font-extralight"
-							key={headerGroup.id}
-						>
-							{headerGroup.headers.map((header) => (
-								<th key={header.id}>
-									{header.index > 1 &&
-									header.id !== "actions" ? (
-										<div className="flex px-1 justify-between items-center w-fit">
-											{flexRender(
-												header.column.columnDef.header,
-												header.getContext(),
-											)}
-											<span className="flex flex-col">
-												<BiChevronUp
-													onClick={header.column.getToggleSortingHandler()}
-													size={24}
-													className="relative top-2 text-slate-400"
-												/>
-												<BiChevronDown
-													onClick={header.column.getToggleSortingHandler()}
-													size={24}
-													className="relative bottom-[7px]"
-												/>
-											</span>
-										</div>
-									) : (
-										<span className="px-2">
-											{flexRender(
-												header.column.columnDef.header,
-												header.getContext(),
-											)}
-										</span>
-									)}
-								</th>
-							))}
-						</tr>
-					))}
-				</thead>
-				<tbody className="">
-					{table.getRowModel().rows.map((row) => {
-						return (
+			<div className="my-10 w-full  h-[85vh] pb-2 bg-white overflow-auto rounded-md border shadow-sm border-slate-300">
+				<header className="flex sticky top-0 justify-between items-center border-b border-slate-300 text-afruna-blue bg-white ">
+					<h1 className="p-3 font-bold ">Orders</h1>
+					<div className="flex justify-between items-center p-3 space-x-2">
+						<InputLabel
+							getValue={(val) => console.log("")}
+							placeholder={"Search"}
+							inputClassName="text-sm p-1"
+							inputsuffixIcon={
+								<button>
+									<MdSearch />
+								</button>
+							}
+							type={"text"}
+						/>
+						<SelectPicker
+							triggerClassName="relative top-[5px] flex w-24 justify-between items-center space-x-1 text-[12px] p-[11px] border border-afruna-gray/30 rounded-md"
+							placeholder="Months"
+							getSelected={(val) => console.log(val)}
+							items={[
+								"All",
+								"January",
+								"Febuary",
+								"March",
+								"April",
+							]}
+						/>
+						<SelectPicker
+							triggerClassName="relative top-[5px] flex w-24 justify-between items-center space-x-1 text-[12px] p-[11px] border border-afruna-gray/30 rounded-md"
+							placeholder="Select"
+							getSelected={(val) => console.log(val)}
+							items={[
+								"This Month",
+								"3 Days",
+								"1 Week",
+								"2 Months",
+								"6 Months",
+							]}
+						/>
+					</div>
+				</header>
+				<table className="w-screen lg:w-full px-8 relative">
+					<thead className=" sticky top-16 bg-white">
+						{table.getHeaderGroups().map((headerGroup) => (
 							<tr
-								className="text-left odd:border-y-[1px] odd:border-slate-300 text-afruna-blue text-[12px]"
-								key={row.id}
+								className="text-left text-afruna-gray text-[12px] font-extralight"
+								key={headerGroup.id}
 							>
-								{row.getVisibleCells().map((cell) => {
-									return (
-										<td className="py-2" key={cell.id}>
-											{flexRender(
-												cell.column.columnDef.cell,
-												cell.getContext(),
-											)}
-										</td>
-									);
-								})}
+								{headerGroup.headers.map((header) => (
+									<th key={header.id}>
+										{header.index > 1 &&
+										header.id !== "actions" ? (
+											<div className="flex px-1 justify-between items-center w-fit">
+												{flexRender(
+													header.column.columnDef
+														.header,
+													header.getContext(),
+												)}
+												<span className="flex flex-col">
+													<BiChevronUp
+														onClick={header.column.getToggleSortingHandler()}
+														size={24}
+														className="relative top-2 text-slate-400"
+													/>
+													<BiChevronDown
+														onClick={header.column.getToggleSortingHandler()}
+														size={24}
+														className="relative bottom-[7px]"
+													/>
+												</span>
+											</div>
+										) : (
+											<span className="px-2">
+												{flexRender(
+													header.column.columnDef
+														.header,
+													header.getContext(),
+												)}
+											</span>
+										)}
+									</th>
+								))}
 							</tr>
-						);
-					})}
-				</tbody>
-			</table>
-		</div>
-		<Pagination table={table} />
+						))}
+					</thead>
+					<tbody className="">
+						{table.getRowModel().rows.map((row) => {
+							return (
+								<tr
+									className="text-left odd:border-y-[1px] odd:border-slate-300 text-afruna-blue text-[12px]"
+									key={row.id}
+								>
+									{row.getVisibleCells().map((cell) => {
+										return (
+											<td className="py-2" key={cell.id}>
+												{flexRender(
+													cell.column.columnDef.cell,
+													cell.getContext(),
+												)}
+											</td>
+										);
+									})}
+								</tr>
+							);
+						})}
+					</tbody>
+				</table>
+			</div>
+			<Pagination table={table} />
 		</main>
 	);
 });
 
-function createPaginationWithCustomType<T>() {
-	// Create a new type that extends CustomType with an additional property
-	type TTable = Table<T>;
-
-	// Define a pagination component that uses the extended table type [TTable]
-	return ({ table }: { table: TTable }) => {
-		const pageCount = table.getPageCount();
-		const next = table.nextPage;
-		const previous = table.previousPage;
-		const canGoBackward = !table.getCanPreviousPage();
-		const canGoForward = !table.getCanNextPage();
-		return (
-			<div className="h-fit mt-2 absolute right-0 pb-20">
-				<div className="flex text-md text-afruna-blue items-center gap-2">
-					<button
-						className="w-7 h-7 bg-white rounded p-1"
-						onClick={()=> table.setPageIndex(0)}
-						disabled={!canGoBackward}
-						>
-						<RxDoubleArrowLeft />
-					</button>
-					<button
-						className="w-7 h-7 bg-white rounded p-1"
-						onClick={previous}
-						disabled={!canGoBackward}
-						>
-						<RxChevronLeft />
-					</button>
-<button
-						className="w-7 h-7 text-sm bg-white rounded p-1"
-						
-						
-						>
-						{table.getPageCount()}
-					</button>
-					<button
-						className="w-7 h-7 bg-white rounded p-1"
-						onClick={next}
-						disabled={!table.getCan}
-						>
-						<RxChevronRight/>
-					</button>
-					<button
-						className="w-7 h-7 bg-white rounded p-1"
-						onClick={()=> table.setPageIndex(table.getPageCount() -1)}
-						disabled={!canGoForward}
-					>
-						<RxDoubleArrowRight />
-					</button>
-				</div>
-			</div>
-		);
-	};
-}
-
-const Pagination = memo(createPaginationWithCustomType<IOrder>());
+const Pagination = createPaginationWithCustomType<IOrder>();
---
+++ src/utils/createPaginationWithCustomType.tsx
@@ -0,0 +1,110 @@
+import { Table } from "@tanstack/react-table";
+import {
+	RxChevronLeft,
+	RxChevronRight,
+	RxDoubleArrowLeft,
+	RxDoubleArrowRight,
+} from "react-icons/rx";
+
+export function createPaginationWithCustomType<T>() {
+	// Create a new type that extends CustomType with an additional property
+	type TTable = Table<T>;
+
+	// Define a pagination component that uses the extended table type [TTable]
+	return ({ table }: { table: TTable }) => {
+		const pageIndex = table.getState().pagination.pageIndex + 1;
+		const pageCount = table.getPageCount();
+		const next = table.nextPage;
+		const previous = table.previousPage;
+		const canGoBackward = !table.getCanPreviousPage();
+		const canGoForward = !table.getCanNextPage(); //if true negate it to avoid the disable-btn-atribut from be active so when it false it the btn becomes disable
+		return (
+			<div className="h-fit mt-2 absolute right-0 pb-20">
+				<div className="flex text-md text-afruna-blue items-center gap-2">
+					<button
+						className="w-7 h-7 bg-white rounded p-1"
+						onClick={() => table.setPageIndex(0)}
+						disabled={canGoBackward}
+					>
+						<RxDoubleArrowLeft />
+					</button>
+					<button
+						className="w-7 h-7 bg-white rounded p-1"
+						onClick={previous}
+						disabled={canGoBackward}
+					>
+						<RxChevronLeft />
+					</button>
+					{pageCount > 3 && (
+						<>
+							<span
+								className={`w-7 h-7 text-sm bg-white rounded p-1`}
+							>
+								...
+							</span>
+							<button
+								onClick={() =>
+									table.setPageIndex(pageCount - 1)
+								}
+								className={`w-7 h-7 text-sm bg-white rounded p-1 ${
+									pageIndex === pageCount &&
+									"bg-blue-950 text-white"
+								}`}
+							>
+								{pageCount}
+							</button>
+						</>
+					)}
+					{pageCount === 3 && (
+						<>
+							<button
+								onClick={() => table.setPageIndex(0)}
+								className={`w-7 h-7 text-sm bg-white rounded p-1 ${
+									pageIndex === 1 && "bg-blue-950 text-white"
+								}`}
+							>
+								1
+							</button>
+							<button
+								onClick={() => table.setPageIndex(1)}
+								className={`w-7 h-7 text-sm bg-white rounded p-1 ${
+									pageIndex === 2 && "bg-blue-950 text-white"
+								}`}
+							>
+								2
+							</button>
+							<button
+								onClick={() =>
+									table.setPageIndex(pageCount - 1)
+								}
+								className={`w-7 h-7 text-sm bg-white rounded p-1 ${
+									pageIndex === pageCount &&
+									"bg-blue-950 text-white"
+								}`}
+							>
+								{pageCount}
+							</button>
+						</>
+					)}
+
+					<button
+						className="w-7 h-7 bg-white rounded p-1"
+						onClick={next}
+						disabled={canGoForward}
+					>
+						<RxChevronRight />
+					</button>
+					<button
+						className="w-7 h-7 bg-white rounded p-1"
+						onClick={() =>
+							table.setPageIndex(table.getPageCount() - 1)
+						}
+						disabled={canGoForward}
+					>
+						<RxDoubleArrowRight />
+					</button>
+				</div>
+			</div>
+		);
+	};
+}
